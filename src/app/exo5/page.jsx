313b9de2eff789ab2c49e784fa3b65f2b76d4430
Quentin Hermiteau <quentinhermiteau@gmail.com>
2024-10-10 12:22:15 +0200

    add exercise 5

---
+++ src/app/exo5/page.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+export default function App() {
+  const count = 0;
+
+  return (
+    <main>
+      <div id="notice">
+        Utilise le hook useState pour faire fonctionner ce compteur
+      </div>
+      <span>{count}</span>
+      <div>
+        <button>-</button>
+        <button>+</button>
+      </div>
+    </main>
+  );
+}
