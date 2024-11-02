8e745984632cf27f37ad301ffd2788d51c097113
toto <toto@gmail.com>
2024-11-02 13:43:46 +0100

    add exercise 13

---
+++ src/app/exercices/13/page.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+const initialState = {
+  past: [],
+  present: 0,
+  future: [],
+};
+
+function reducer(state, action) {
+  const { past, present, future } = state;
+
+  return state;
+}
+
+export default function CounterWithUndoRedo() {
+  const state = initialState;
+
+  const handleIncrement = () => {};
+  const handleDecrement = () => {};
+  const handleUndo = () => {};
+  const handleRedo = () => {};
+
+  return (
+    <div>
+      <h1>Counter: {state.present}</h1>
+      <button className="link" onClick={handleIncrement}>
+        Increment
+      </button>
+      <button className="link" onClick={handleDecrement}>
+        Decrement
+      </button>
+      <button
+        className="link"
+        onClick={handleUndo}
+        disabled={!state.past.length}
+      >
+        Undo
+      </button>
+      <button
+        className="link"
+        onClick={handleRedo}
+        disabled={!state.future.length}
+      >
+        Redo
+      </button>
+    </div>
+  );
+}
