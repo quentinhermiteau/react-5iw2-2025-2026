7055b97f6d4fe41b878c399c04479bc14ea20447
Quentin Hermiteau <quentinhermiteau@gmail.com>
2024-11-04 21:34:59 +0100

    refacto navbar

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,5 +1,3 @@
-import { readdirSync } from "fs";
-
 function Link({ exercice }) {
   return (
     <li style={{ padding: "5px 0" }}>
@@ -9,16 +7,40 @@ function Link({ exercice }) {
 }
 
 export default function Navbar() {
-  const exercices = readdirSync("./src/app/exercices").map((folder) => folder);
-
-  exercices.sort((a, b) => a - b);
+  const themes = [
+    {
+      name: "Intro",
+      exercices: [1, 2, 3],
+    },
+    {
+      name: "useState",
+      exercices: [4, 5, 6, 7, 8],
+    },
+    {
+      name: "useContext",
+      exercices: [9, 10],
+    },
+    {
+      name: "useReducer",
+      exercices: [11, 12, 13],
+    },
+    {
+      name: "Memoïsation",
+      exercices: [14, 15],
+    },
+  ];
 
   return (
     <div id="navbar">
       <h1>Exercices</h1>
       <ul>
-        {exercices.map((exercice) => (
-          <Link key={exercice} exercice={exercice} />
+        {themes.map(({ name, exercices }) => (
+          <div key={name}>
+            <h2>{name}</h2>
+            {exercices.map((exercice) => (
+              <Link key={exercice} exercice={exercice} />
+            ))}
+          </div>
         ))}
       </ul>
     </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,3 @@
-import { readdirSync } from "fs";
-
 export default function Home() {
-  const exercices = readdirSync("./src/app/exercices").map((folder) => folder);
-  console.log(exercices);
-
-  return (
-    <div>
-      <h1>Exercices</h1>
-      <ul>
-        {exercices.map((exercice) => (
-          <li key={exercice}>
-            <a href={`/exercices/${exercice}`}>{exercice}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
