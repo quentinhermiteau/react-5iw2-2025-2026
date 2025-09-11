589a58c5737a5568e14467054dfa9c11af286760
Quentin Hermiteau <quentinhermiteau@gmail.com>
2025-09-11 10:50:08 +0200

    démo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <div></div>;
+  const isTrue = true;
+  const isFalse = false;
+
+  if (isTrue && !isFalse) {
+  }
+
+  return (
+    <>
+      {isTrue ? <p>C'est vrai</p> : <p>C'est pas vrai</p>}
+      {isTrue && <p>Ça fonctionne</p>}
+      <img className="toto" src="" />
+    </>
+  );
 }
