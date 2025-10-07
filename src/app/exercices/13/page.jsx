ab84b06990f63ccd459628aaf9e40c4377821372
Quentin Hermiteau <quentinhermiteau@gmail.com>
2025-10-07 12:43:59 +0200

    correction exo 13

--- src/app/exercices/13/page.jsx
+++ src/app/exercices/13/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useReducer } from "react";
+
 const initialState = {
   past: [],
   count: 0,
@@ -9,25 +11,67 @@ const initialState = {
 function reducer(state, action) {
   const { past, count, future } = state;
 
-  return state;
+  // past = []
+  // count = 0
+  // future = []
+  switch (action) {
+    // past = [0]
+    // count = 1
+    // future = []
+    case "increment":
+      return { ...state, count: count + 1, past: [...past, count] };
+    case "decrement":
+      return { ...state, count: count - 1, past: [...past, count] };
+
+    // past = [0, 1, 2, 3]
+    // count = 4
+    // future = []
+    case "undo":
+      // past = [0, 1, 2]
+      // count = 3
+      // future = [4]
+      const undoCount = past.at([-1]);
+      const undoPast = past.slice(0, -1);
+      const undoFuture = [...future, count];
+
+      return { ...state, count: undoCount, past: undoPast, future: undoFuture };
+
+    // past = [0, 1, 2]
+    // count = 3
+    // future = [5, 4]
+    case "redo":
+      // past = [0, 1, 2, 3]
+      // count = 4
+      // future = [5]
+      const redoCount = future.at([-1]);
+      const redoPast = [...past, count];
+      const redoFuture = future.slice(0, -1);
+
+      return { ...state, count: redoCount, past: redoPast, future: redoFuture };
+
+    default:
+      break;
+  }
 }
 
 export default function CounterWithUndoRedo() {
-  const state = initialState;
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleIncrement = () => {};
-  const handleDecrement = () => {};
-  const handleUndo = () => {};
-  const handleRedo = () => {};
+  console.log(state);
+
+  const handleIncrement = () => dispatch("increment");
+  const handleDecrement = () => dispatch("decrement");
+  const handleUndo = () => dispatch("undo");
+  const handleRedo = () => dispatch("redo");
 
   return (
     <div>
       <h1>Counter: {state.count}</h1>
-      <button className="link" onClick={handleIncrement}>
-        Increment
-      </button>
       <button className="link" onClick={handleDecrement}>
         Decrement
+      </button>
+      <button className="link" onClick={handleIncrement}>
+        Increment
       </button>
       <button
         className="link"
