cef2782da1ef5f56bed63ffaf6e5f4f7e52bf540
Quentin Hermiteau <quentinhermiteau@gmail.com>
2025-10-07 09:34:52 +0200

    correction démo 4

--- src/app/demo/4/page.jsx
+++ src/app/demo/4/page.jsx
@@ -1,10 +1,49 @@
 "use client";
 
+import { useReducer } from "react";
+
+const initialForm = {
+  fields: {
+    name: "",
+    email: "",
+    cgu: false,
+  },
+  submitting: false,
+  success: false,
+  error: null,
+};
+
+const formReducer = (state, { type, payload }) => {
+  switch (type) {
+    case "SET_SUBMITTING":
+      return { ...state, submitting: payload };
+    case "SET_SUCCESS":
+      return { ...state, success: payload };
+    case "SET_ERROR":
+      return { ...state, error: payload };
+    case "SET_FIELD":
+      return {
+        ...state,
+        fields: { ...state.fields, [payload.key]: payload.value },
+      };
+  }
+};
+
 export default function App() {
   // Gérer l'état du formulaire avec useState puis refacto avec useReducer
+  const [form, dispatch] = useReducer(formReducer, initialForm);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    dispatch({ type: "SET_SUBMITTING", payload: true });
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      dispatch({ type: "SET_SUCCESS", payload: true });
+    } catch (error) {
+      dispatch({ type: "SET_ERROR", payload: error.message });
+    } finally {
+      dispatch({ type: "SET_SUBMITTING", payload: false });
+    }
   };
 
   return (
@@ -17,8 +56,13 @@ export default function App() {
             id="name"
             name="name"
             type="text"
-            onChange={(e) => {}}
-            value=""
+            onChange={(e) => {
+              dispatch({
+                type: "SET_FIELD",
+                payload: { key: e.target.name, value: e.target.value },
+              });
+            }}
+            value={form.fields.name}
             required
             placeholder="Your name"
           />
@@ -27,8 +71,13 @@ export default function App() {
             id="email-address"
             name="email"
             type="email"
-            onChange={(e) => {}}
-            value=""
+            onChange={(e) => {
+              dispatch({
+                type: "SET_FIELD",
+                payload: { key: e.target.name, value: e.target.value },
+              });
+            }}
+            value={form.fields.email}
             autoComplete="email"
             required
             placeholder="Email Address"
@@ -41,15 +90,20 @@ export default function App() {
             id="cgu"
             name="cgu"
             type="checkbox"
-            onChange={(e) => {}}
-            checked={false}
+            onChange={(e) => {
+              dispatch({
+                type: "SET_FIELD",
+                payload: { key: e.target.name, value: e.target.checked },
+              });
+            }}
+            checked={form.fields.cgu}
           />
           <p>I agree to everything.</p>
         </div>
       </form>
-      {submitting && <p>Submitting...</p>}
-      {error && <p>{error}</p>}
-      {success && <p>Success!</p>}
+      {form.submitting && <p>Submitting...</p>}
+      {form.error && <p>{form.error}</p>}
+      {form.success && <p>Success!</p>}
     </div>
   );
 }
